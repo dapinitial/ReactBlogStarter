963e170b25a2656abd674124dd36e71293a8890e
David Puerto <me@davidpuerto.com>
2017-05-31 01:52:44 -0700

    add forms for new posts

--- src/components/posts-index.js
+++ src/components/posts-index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -19,10 +20,13 @@ class PostsIndex extends Component {
   }
 
   render() {
-    // console.log(this.props.posts);
+    //console.log(this.props.posts);
     return (
       <div>
-        <h3>Posts Index</h3>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">Add a Post</Link>
+        </div>
+        <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
         </ul>
--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,1 +1,33 @@
-posts-new.js
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          className="form-control"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    return(
+      <form>
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+      </form>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
