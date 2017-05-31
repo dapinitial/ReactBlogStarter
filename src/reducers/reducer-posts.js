2431862b34fbf9547ae6f949a8c9ffa5c0bfce59
David Puerto <me@davidpuerto.com>
2017-05-31 16:42:43 -0700

    get post action creator and logic for the reducer

--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch(action.type) {
   case FETCH_POSTS:
-  return _.mapKeys(action.payload.data, 'id');
+    const post = action.payload.data;
+    // key interpolation, not creating an array!!! see comment below (ES5 example);
+    return { ...state, [action.payload.data.id]: action.payload.data };
+    // return { ...state }
+    // newState[post.id] = post;
+    // return newState;
+  case FETCH_POSTS:
+    return _.mapKeys(action.payload.data, 'id');
   default:
     return state;
   }
